41845c40a92df438f7e7a4d78e883da351de7e7a
dmaddaloni <61094837+dmaddaloni@users.noreply.github.com>
2020-02-26 22:07:30 -0500

    Delete Titanic Histograms & Bar Charts.R

--- Titanic Histograms & Bar Charts.R
+++
@@ -1,7 +0,0 @@
-hist(titanic$Age, main = "Age of Passengers & Crew Members", xlab = "Age")
-
-hist(titanic$Fare, main = "Cost of Each Ticket", xlab = "Fare")
-
-barplot(table(titanic$Sex), main = "Sex of Passengers & Crew Members", xlab = "Sex")
-
-barplot(table(titanic$Survived), main = "How Many Survived?", xlab = "# of Survivors vs Non-Survivors")
