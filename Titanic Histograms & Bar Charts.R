3094a3cae83cf4cc88856cd53cadd870813e9594
dmaddaloni <61094837+dmaddaloni@users.noreply.github.com>
2020-02-26 22:07:51 -0500

    R Codes

---
+++ Titanic Histograms & Bar Charts.R
@@ -0,0 +1,7 @@
+hist(titanic$Age, main = "Age of Passengers & Crew Members", xlab = "Age", ylab = "Number of People")
+
+hist(titanic$Fare, main = "Cost of Each Ticket", xlab = "Fare", ylab = "Number of People")
+
+barplot(table(titanic$Survived), main = "How Many Survived?", xlab = "Non-Survivor (0)   vs.   Survivor (1)", ylab = "Number of People")
+
+barplot(table(titanic$Sex), main = "Sex of Passengers & Crew Members", xlab = "Sex", ylab = "Number of People")
