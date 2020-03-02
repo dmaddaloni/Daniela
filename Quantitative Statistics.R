31642ea5f02bbe285b28418e2a209e46f8e49e99
dmaddaloni <61094837+dmaddaloni@users.noreply.github.com>
2020-03-02 15:23:02 -0500

    Stats of Age & Fares data

---
+++ Quantitative Statistics.R
@@ -0,0 +1,9 @@
+var(titanic$Age, na.rm=TRUE)
+mean(titanic$Age, na.rm=TRUE)
+median(titanic$Age, na.rm=TRUE)
+sd(titanic$Age, na.rm=TRUE)
+
+mean(titanic$Fare)
+median(titanic$Fare)
+var(titanic$Fare)
+sd(titanic$Fare)
